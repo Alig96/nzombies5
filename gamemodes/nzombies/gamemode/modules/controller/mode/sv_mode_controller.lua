9bd72bb1aaceccbd5ba7bf9b16dd2e191bb7a657
Alig96 <aliaslam_10@hotmail.co.uk>
2018-02-11 14:31:43 +0000

    Adapted mode controller to use the new syntax

--- gamemodes/nzombies/gamemode/modules/controller/mode/sv_mode_controller.lua
+++ gamemodes/nzombies/gamemode/modules/controller/mode/sv_mode_controller.lua
@@ -1,40 +1,23 @@
-local logModuleName = "Controller:Mode"
-
-local function authorizeRequest(ply)
+local function authorizeRequest(controllerObject, ply)
   if ply:IsSuperAdmin() then
     return true
-  else
-    Log(LOG_ERROR, "Could not change mode since " .. ply:Nick() .. " is not authorised.", logModuleName)
-    return false
   end
 end
 
-local function validateRequest(requestData)
+local function validateRequest(controllerObject, requestData)
   local requestedMode = requestData["requestedMode"]
 
   if requestedMode == MODE_PLAY or requestedMode == MODE_CREATIVE then
     return true
   end
 
-  Log(LOG_ERROR, "Could not change mode since the data supplied did not pass validation.", logModuleName)
   return false
 end
 
--- Handles all incoming requests from a player
-function nz.Controller.handleChangeMode(ply, requestData)
-  -- Check the user is authorised to do this action
-  local authorised = authorizeRequest(ply)
-  -- Validate the request
-  local validated = validateRequest(requestData)
-
-  -- If both tests pass
-  if authorised and validated then
-    Log(LOG_INFO, ply:Nick() ..  " has made a successful request to change the mode.", logModuleName)
-    -- Execute the requests
-    nz.Mode:set(requestData["requestedMode"])
-
-    return true
-  end
-
-  return false
+local function onSuccess(controllerObject, requestingPlayer, requestData)
+  local requestedModeNumber = requestData["requestedMode"]
+  nz.Mode:set(requestedModeNumber)
+  Log(LOG_INFO, requestingPlayer:Nick() ..  " has changed the mode to: " .. requestedModeNumber .. ".", "Controller:" .. controllerObject.id)
 end
+
+nz.Controller.handleChangeMode = nz.Framework.createController("ChangeMode", onSuccess, authorizeRequest, validateRequest)
