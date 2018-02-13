de3c422eade65e17c0d039c4775bbd19e0851f3c
Alig96 <aliaslam_10@hotmail.co.uk>
2018-02-13 23:02:54 +0000

    Re-added the scoreboard but tweaked to only be visible in play mode.

---
+++ gamemodes/nzombies/gamemode/modules/display/colors/sh_constructor.lua
@@ -0,0 +1,19 @@
+-- Constructor
+nz.Colors = {}
+
+-- For now just throw them here
+
+-- Colours
+nz.Colours = {}
+
+nz.Colours["primary"] = Color(249, 88, 13)
+nz.Colours["secondary"] = Color(218, 187, 15)
+
+nz.Colours["success"] = Color(76, 175, 80)
+nz.Colours["info"] = Color(3, 169, 244)
+nz.Colours["warning"] = Color(255, 87, 34)
+nz.Colours["danger"] = Color(244, 67, 54)
+
+nz.Colours["white"] = Color(236, 240, 241)
+nz.Colours["grey"] = Color(127, 140, 141)
+nz.Colours["darkGreen"] = Color(22, 160, 133)
